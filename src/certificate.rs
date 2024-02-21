4c2c28add02b1b12b9cd98d2d52f0c8b44281a6b
René Rössler <rene@freshx.de>
2024-02-21 20:01:19 +0100

    Rename sn to cn and add ou (#8)
    * rename subject_name to common_name
    
    * add organizational unit
    
    * make common_name and organizational_unit optional

--- src/certificate.rs
+++ src/certificate.rs
@@ -6,18 +6,34 @@ use crate::Result;
 /// Certificate information for a TLS connection.
 #[derive(Debug)]
 pub struct Certificate {
-    subject_name: String,
+    //TODO: Change to Option when `subject_name` is removed.
+    common_name: String,
+    organizational_unit: Option<String>,
 }
 
 impl Certificate {
     /// Creates a new certificate.
-    pub(crate) fn new(subject_name: String) -> Certificate {
-        Certificate { subject_name }
+    pub(crate) fn new(common_name: String, organizational_unit: Option<String>) -> Certificate {
+        Certificate {
+            common_name,
+            organizational_unit,
+        }
     }
 
-    /// Returns the subject name of the certificate.
+    /// Returns the common name of the certificate.
+    #[deprecated(note = "please use `common_name` instead")]
     pub fn subject_name(&self) -> &str {
-        &self.subject_name
+        &self.common_name
+    }
+
+    /// Returns the common name of the certificate.
+    pub fn common_name(&self) -> Option<&str> {
+        Some(&self.common_name)
+    }
+
+    /// Returns the organizational unit of the certificate.
+    pub fn organizational_unit(&self) -> Option<&str> {
+        self.organizational_unit.as_deref()
     }
 }
 
