61652b20b59813a92c495432ef94af435a879fab
René Rössler <rene@freshx.de>
2024-03-22 09:40:17 +0100

    clippy lints

--- tests/server.rs
+++ tests/server.rs
@@ -4,15 +4,12 @@ use rstest::*;
 use std::{net::SocketAddr, sync::Arc};
 use tokio::sync::oneshot;
 use warp_openssl::Result;
-use warp_openssl::{CertificateVerifier, serve};
+use warp_openssl::{serve, CertificateVerifier};
 
 struct ValidCertVerifier {}
 
 impl CertificateVerifier for ValidCertVerifier {
-    fn verify_certificate(
-        &self,
-        _: &warp_openssl::Certificate,
-    ) -> warp_openssl::Result<()> {
+    fn verify_certificate(&self, _: &warp_openssl::Certificate) -> warp_openssl::Result<()> {
         Result::Ok(())
     }
 }
@@ -20,10 +17,7 @@ impl CertificateVerifier for ValidCertVerifier {
 struct InValidCertVerifier {}
 
 impl CertificateVerifier for InValidCertVerifier {
-    fn verify_certificate(
-        &self,
-        _: &warp_openssl::Certificate,
-    ) -> warp_openssl::Result<()> {
+    fn verify_certificate(&self, _: &warp_openssl::Certificate) -> warp_openssl::Result<()> {
         Result::Err("Invalid certificate".into())
     }
 }
@@ -91,31 +85,26 @@ async fn client_tests(
     let mut host_cert = include_bytes!("../certs/localhost.crt").to_vec();
     host_cert.extend(ca_cert.clone());
 
-    
     let intermediate_cert = include_bytes!("../certs/intermediate.crt").to_vec();
-    
+
     let (tx, rx) = oneshot::channel::<()>();
     let server = serve(warp::Filter::map(warp::any(), || "Hello, World!"))
-        .key(include_bytes!("../certs/localhost.key").to_vec())
+        .key(include_bytes!("../certs/localhost.key"))
         .cert(host_cert);
 
     let server = match auth_type {
         AuthType::Off => server,
         AuthType::Required => match verifier_type {
-            VeriferType::Valid => {
-                server.client_auth_required(intermediate_cert.clone(), Arc::new(ValidCertVerifier {}))
-            }
-            VeriferType::Invalid => {
-                server.client_auth_required(intermediate_cert.clone(), Arc::new(InValidCertVerifier {}))
-            }
+            VeriferType::Valid => server
+                .client_auth_required(intermediate_cert.clone(), Arc::new(ValidCertVerifier {})),
+            VeriferType::Invalid => server
+                .client_auth_required(intermediate_cert.clone(), Arc::new(InValidCertVerifier {})),
         },
         AuthType::Optional => match verifier_type {
-            VeriferType::Valid => {
-                server.client_auth_optional(intermediate_cert.clone(), Arc::new(ValidCertVerifier {}))
-            }
-            VeriferType::Invalid => {
-                server.client_auth_optional(intermediate_cert.clone(), Arc::new(InValidCertVerifier {}))
-            }
+            VeriferType::Valid => server
+                .client_auth_optional(intermediate_cert.clone(), Arc::new(ValidCertVerifier {})),
+            VeriferType::Invalid => server
+                .client_auth_optional(intermediate_cert.clone(), Arc::new(InValidCertVerifier {})),
         },
     };
 
@@ -129,12 +118,10 @@ async fn client_tests(
 
     let crt = include_bytes!("../certs/client.crt").to_vec();
     let key = include_bytes!("../certs/client.key").to_vec();
-    let identity = Identity::from_pem(
-        &[key, crt].concat(),
-    )?;
+    let identity = Identity::from_pem(&[key, crt].concat())?;
 
     let trust_root = Certificate::from_pem(&ca_cert).unwrap();
-    
+
     for version in [Version::TLS_1_2, Version::TLS_1_3] {
         println!("Testing with version: {:?}", version);
 
@@ -150,24 +137,22 @@ async fn client_tests(
         } else {
             builder
         };
-    
+
         let client = builder.build()?;
         let res = client
             .get(format!("https://localhost:{}", addr.port()))
             .send()
             .await;
-    
+
         println!("Response: {:?}", res);
-        
+
         if let Ok(ref res) = res {
-            assert_eq!(expect_error, false);
+            assert!(!expect_error);
             assert_eq!(res.status(), 200);
         } else {
-            assert_eq!(expect_error, true);
-            
+            assert!(expect_error);
         }
     }
-
 
     tx.send(()).unwrap();
     server.await.unwrap();
