ef948b1bfb8d51c0a96e5cf382e93430133944ce
René Rössler <rene@freshx.de>
2024-04-03 05:39:27 +0200

    Certificate in filter (#12)
    * clippy lints
    
    * change Certificate to implement TryFrom<X509>
    
    * Add Certificate to extension to get it in filter
    
    * Add myself to authors
    
    * use io::Result

--- examples/server.rs
+++ examples/server.rs
@@ -1,24 +1,20 @@
+use std::{net::SocketAddr, sync::Arc};
 use tokio::signal::ctrl_c;
+use tokio::sync::oneshot;
 use tracing::info;
 use tracing_subscriber::EnvFilter;
-use std::{net::SocketAddr, sync::Arc};
-use tokio::sync::oneshot;
 use warp_openssl::Result;
-use warp_openssl::{CertificateVerifier, serve};
+use warp_openssl::{serve, CertificateVerifier};
 
 struct ValidCertVerifier {}
 
 impl CertificateVerifier for ValidCertVerifier {
-    fn verify_certificate(
-        &self,
-        _: &warp_openssl::Certificate,
-    ) -> warp_openssl::Result<()> {
+    fn verify_certificate(&self, _: &warp_openssl::Certificate) -> warp_openssl::Result<()> {
         Result::Ok(())
     }
 }
 
 use clap::Parser;
-
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -36,24 +32,26 @@ struct Args {
 /// Requires openssl to be installed.
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt().with_env_filter(EnvFilter::from_default_env()).init();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
 
     let args = Args::parse();
-    
+
     let addr = SocketAddr::from(([127, 0, 0, 1], args.port));
     let ca_cert = include_bytes!("../certs/ca.crt").to_vec();
     let mut host_cert = include_bytes!("../certs/localhost.crt").to_vec();
     host_cert.extend(ca_cert.clone());
 
-    
     let intermediate_cert = include_bytes!("../certs/intermediate.crt").to_vec();
-    
+
     let (tx, rx) = oneshot::channel::<()>();
     let server = serve(warp::Filter::map(warp::any(), || "Hello, World!"))
-        .key(include_bytes!("../certs/localhost.key").to_vec())
+        .key(include_bytes!("../certs/localhost.key"))
         .cert(host_cert);
 
-    let server = server.client_auth_required(intermediate_cert.clone(), Arc::new(ValidCertVerifier {}));
+    let server =
+        server.client_auth_required(intermediate_cert.clone(), Arc::new(ValidCertVerifier {}));
 
     let (addr, server) = server.bind_with_graceful_shutdown(addr, async move {
         rx.await.ok();
